0e5c6412b6f7edc1ee5abcdd23c7fd02baea0d2f
arya <2024asap22@gmail.com>
2025-06-17 01:34:24 -0400

    Added routes and an array to store image schema objects in a basic way
    I added update_images, and updated get_images and create_images(). An array, my_images, is used to store images in memory.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from app.schemas import Image
 from dotenv import load_dotenv
 
+from random import randrange
 
 
 app = FastAPI()
@@ -10,7 +11,7 @@ app = FastAPI()
 
 
 
-
+my_images = [{"title": "title of image 1", "content": "content of image 1", "id": 1}, {"title": "favorite foods", "content": "i like pizza", "id": 2}]
 
 # What we need for building CRUD
 # Database, FastAPI, Pycopg
@@ -21,14 +22,34 @@ def root():
 
 @app.get("/images")
 def get_images():
-    return {"data": "This are your images"}
+    return {"data": my_images}
 
 
 
-@app.post("/createimages")
-def create_images(new_image: Image):
-    print(new_image)
-    return {"data": "new image"}
+@app.post("/images")
+def create_images(image: Image):
+    # Take the new_image and store it in the database. Then, return the new image with the id as a dict
+    image_dict = image.dict()
+    image_dict['id'] = randrange(0,1000000)
+    return image_dict
+
+
+
+@app.put("/images/{id}")
+def update_images(id: int, image: Image):
+    # find the image with the matching id
+
+    for i, item in enumerate(my_images):
+        if(item['id'] == id):
+            updated_image = image.dict() # Create a dict representation first, separately to do multiple operations on (and make cleaner)
+            updated_image['id'] = id    # Safely insert id
+            my_images[i] = updated_image # Finally, set the array index to the updated image
+            return updated_image
+    
+    return {"error": "Image not found"}
+    # manipulate the image
+    # return the updated image as a dictionary
+
 
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
+
 
 
 class Image(BaseModel):
     filename: str
     content: str
-    time_created: datetime
+    time_created: datetime = datetime.now()
+    rating: Optional[int] = None
 
 
